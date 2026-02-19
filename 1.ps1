a545eb6a398a1f6f86e0b7cc1ffd9e1e719b6af3
Bibon <165363279+bibonuwu@users.noreply.github.com>
2026-02-19 11:41:31 +0500

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,12 +1,1 @@
-server {
-    server_name abekenaibek.kz;
-    root /var/www/abekenaibek.kz;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-
-    types {
-        text/plain ps1;
-    }
-}
+Write-Host "irm https://get.activated.win | iex"
